afb2671ccc9bdb567110ad40f2f290f79f60c66a
Krishna <chkp1999@gmail.com>
2025-01-27 11:50:56 -0500

    Docker file added

--- src/main/java/com/employee/portal/PortalApplication.java
+++ src/main/java/com/employee/portal/PortalApplication.java
@@ -2,12 +2,18 @@ package com.employee.portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class PortalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortalApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String home() {
+		return "Dockerized Emp Portal Web App";
 	}
 
 }
